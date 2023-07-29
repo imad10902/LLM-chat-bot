98c4f6245a064a013d2102848c531dc6f194e97e
imad10902 <imadbinasad1@gmail.com>
2023-07-29 13:27:37 +0530

    Refactor

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, g
 from flask_cors import CORS
 import config
-from aiapi import ChatService
+from chat_service import ChatService
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
@@ -16,6 +16,56 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+@app.post('/chat')
+def home():
+    user_prompt = request.get_json()['message']
+    userid = request.get_json()['user_id']
+    sender_name = request.get_json()['sender']
+    try:
+        chat_history = get_chat_history(userid, user_prompt, sender_name)
+        response = ChatService.generateChatResponse(chat_history)
+        
+        res = {'sender': 'Bot', "message": response}
+        
+        new_chat = Chat(
+            userid = userid,
+            name = sender_name,
+            request = user_prompt,
+            response = response,
+        )
+        
+        db.session.add(new_chat)
+        
+        db.session.commit()
+        
+        return jsonify(res), 200
+    
+    except Exception as e:
+        print(e)
+        if e.code == 'context_length_exceeded':
+            return jsonify({'Error': 'The content length is exceeded'}), 400
+        else:
+            return jsonify({'error': 'The rate limit exceeded'}), 400
+
+
+def get_chat_history(userid, prompt, name):
+    messages = []
+    messages.append(
+        {
+            'role': 'system',
+            'content': Prompts.get_general_prompts(name),
+        }
+    )
+    chats = Chat.query.filter_by(userid = userid).all()
+    chats_as_dicts = [chat.__dict__ for chat in chats]
+    for chat in chats_as_dicts:
+        question = {}
+        question['role'] = 'user'
+        question['content'] = chat['request']
+        messages.append(question)
+    messages.append({'role':'user', 'content': prompt})
+    return messages
+
 class Chat(db.Model):
     id = userid = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, unique=False, nullable=False)
@@ -25,47 +75,8 @@ class Chat(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"<Chat {self.name}>"
-
-
-@app.post("/chat")
-def home():
-    prompt = request.get_json()["message"]
-    userid = request.get_json()["user_id"]
-    messages = []
-    messages.append(
-        {
-            "role": "system",
-            "content": Prompts.get_general_prompts(request.get_json()["sender"]),
-        }
-    )
-    chats = Chat.query.filter_by(userid=userid).all()
-    chats_as_dicts = [chat.__dict__ for chat in chats]
-    for chat in chats_as_dicts:
-        question = {}
-        question["role"] = "user"
-        question["content"] = chat["request"]
-        messages.append(question)
-    messages.append({'role':'user', 'content': prompt})
-    try:
-        response = ChatService.generateChatResponse(messages)
-        res = {"sender": "Bot", "message": response}
-        new_chat = Chat(
-            userid=request.get_json()["uid"],
-            name=request.get_json()["sender"],
-            request=request.get_json()["message"],
-            response=response,
-        )
-        db.session.add(new_chat)
-        db.session.commit()
-        return jsonify(res), 200
-    except Exception as e:
-        print(e)
-        if e.code == "context_length_exceeded":
-            return jsonify({"Error": "The content length is exceeded"}), 400
-        else:
-            return jsonify({"error": "The rate limit exceeded"}), 400
-
+        return f'<Chat {self.name}>'
+    
 
 if __name__ == "__main__":
     app.run()
--- prompts.py
+++ prompts.py
@@ -1,5 +1,9 @@
 class Prompts:
 
     def get_general_prompts(name):
-        prompt = f"You are a general chat bot that answers everything. You have to answer user in accordance to their previous chats.You have to use user's name in converstaion repeatedly. User's name is {name}. You need to answer in human form"
+        prompt = f'''1) You are a general chat bot that answers everything. 
+        2) You have to answer user in accordance to their previous chats.
+        3) You have to use user's name in converstaion repeatedly. 
+        4) User's name is {name}. 
+        5) You need to answer in human-like speech.'''
         return prompt
